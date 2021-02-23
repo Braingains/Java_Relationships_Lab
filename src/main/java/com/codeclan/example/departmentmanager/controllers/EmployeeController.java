1c31ab709b4364c92294869ed5b32e4f8e836e1f
Ewan Brain <ewan.brain@gmail.com>
2021-02-23 10:25:43 +0000

    employee class built

---
+++ src/main/java/com/codeclan/example/departmentmanager/controllers/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.departmentmanager.controllers;
+
+public class EmployeeController {
+}
---
+++ src/main/java/com/codeclan/example/departmentmanager/models/Department.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.departmentmanager.models;
+
+import javax.persistence.Entity;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @Gen
+}
