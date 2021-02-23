5491f12a20e1c17e74976f7d622bb62cb71f4af3
Ewan Brain <ewan.brain@gmail.com>
2021-02-23 10:08:19 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/departmentmanager/DepartmentmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.departmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/departmentmanager/DepartmentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.departmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
